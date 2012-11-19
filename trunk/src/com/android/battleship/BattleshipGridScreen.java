66038b6bbc9b8d26ffb667a33f8f6ba08fd3e8c7
dprinslow@gmail.com <dprinslow@gmail.com@41c4afba-1071-bf11-cc8b-69732f53bf77>
2012-11-19 17:20:57 +0000

    Added starter screens, objects, descriptions...

---
+++ trunk/src/com/android/battleship/BattleshipGridScreen.java
@@ -0,0 +1,13 @@
+package com.android.battleship;
+
+import android.app.Activity;
+
+/**
+ * This will be the main screen with a grid of buttons.
+ * 
+ * @author DPrinslow
+ *
+ */
+public class BattleshipGridScreen extends Activity{
+
+}
---
+++ trunk/src/com/android/battleship/BluetoothManagerScreen.java
@@ -0,0 +1,14 @@
+package com.android.battleship;
+
+import android.app.Activity;
+
+/**
+ * This screen will manage the bluetooth pairing and may turn into a game management screen with different
+ * paired devices.
+ * 
+ * @author DPrinslow
+ *
+ */
+public class BluetoothManagerScreen extends Activity{
+
+}
