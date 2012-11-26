ab2e4ce97d5509e3961c07f62d2d7dd87dc803fa
dprinslow <dprinslow@gmail.com>
2012-11-26 19:54:59 +0000

    Updated bluetooth stuff. Still working on it.

--- src/com/android/battleship/BluetoothClient.java
+++ src/com/android/battleship/BluetoothClient.java
@@ -1,54 +1,60 @@
 package com.android.battleship;
 
-import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.UUID;
 
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
 
-public class BluetoothClient extends Thread {
-    private final BluetoothSocket mmSocket = null;
-    private final BluetoothDevice mmDevice = null;
- 
-    public BluetoothClient(BluetoothDevice device) {
-        // Use a temporary object that is later assigned to mmSocket,
-        // because mmSocket is final
-        BluetoothSocket tmp = null;
-        //mmDevice = device;
- 
-        // Get a BluetoothSocket to connect with the given BluetoothDevice
-        try {
-            // MY_UUID is the app's UUID string, also used by the server code
-            //TODO: tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
-        } catch (Exception e) { 
-        	
-        }
-       // mmSocket = tmp;
-    }
- 
-    public void run() {
-        // Cancel discovery because it will slow down the connection
-        //TODO: mBluetoothAdapter.cancelDiscovery();
- 
-        try {
-            // Connect the device through the socket. This will block
-            // until it succeeds or throws an exception
-            mmSocket.connect();
-        } catch (IOException connectException) {
-            // Unable to connect; close the socket and get out
-            try {
-                mmSocket.close();
-            } catch (IOException closeException) { }
-            return;
-        }
- 
-        // Do work to manage the connection (in a separate thread)
-       //TODO: manageConnectedSocket(mmSocket);
-    }
- 
-    /** Will cancel an in-progress connection, and close the socket */
-    public void cancel() {
-        try {
-            mmSocket.close();
-        } catch (IOException e) { }
-    }
+class BluetoothClient extends Thread {
+
+	BluetoothAdapter mBluetoothAdapter;
+	private String data = null;
+
+	final Handler handler;
+	final Runnable updateUI;
+
+	public BluetoothClient(Handler handler, Runnable updateUI) {
+		this.handler = handler;
+		this.updateUI = updateUI;
+
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	}
+
+	public String getBluetoothClientData() {
+		return data;
+	}
+
+	public void run() {
+		BluetoothSocket clientSocket = null;
+		// Client knows the server MAC address
+		BluetoothDevice mmDevice = mBluetoothAdapter
+				.getRemoteDevice("28:CF:DA:D6:41:5D");
+
+		try {
+			// UUID string same used by server
+			clientSocket = mmDevice.createRfcommSocketToServiceRecord(UUID
+					.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+
+			mBluetoothAdapter.cancelDiscovery(); // Cancel, discovery slows
+													// connection
+
+			clientSocket.connect();
+
+			DataInputStream in = new DataInputStream(
+					clientSocket.getInputStream());
+			DataOutputStream out = new DataOutputStream(
+					clientSocket.getOutputStream());
+
+			out.writeUTF("Hello"); // Send to server
+
+			data = in.readUTF(); // Read from server
+
+			handler.post(updateUI);
+		} catch (Exception e) {
+		}
+	}
 }
