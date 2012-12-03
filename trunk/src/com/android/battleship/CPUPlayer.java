31b915f078a371ea7ade74aa61663ea77847154b
mschabowsky@gmail.com <mschabowsky@gmail.com@41c4afba-1071-bf11-cc8b-69732f53bf77>
2012-12-03 21:49:02 +0000

    See TODO list at top of file.

--- trunk/src/com/android/battleship/CPUPlayer.java
+++ trunk/src/com/android/battleship/CPUPlayer.java
@@ -1,3 +1,14 @@
+/* This class handles all CPU aspects of single player game.
+ * 
+ * TODO: Create method(s) to set computer ships
+ * 		 Create Array to track all computer attempts so attempts are not re-tried.
+ * 		 Integrate into Single Player gameplay
+ * 
+ * Updated: Mon, 12/3 at 4:45 PM by Matt
+ * 
+ */
+
+
 package com.android.battleship;
 
 import java.util.ArrayList;
@@ -9,36 +20,153 @@ import android.util.Log;
 public class CPUPlayer extends Activity{
 
 	private Random generator = new Random( 10 );
-	private boolean useSmartMove = false;
-	private int x, y, lastX, lastY, hitX, hitY, lastCase;
+	private int x, y, lastX, lastY, firstHitX, firstHitY, direction;
 	private int[] hitGuesses = new int[4];
-	private String orientation = "unknown";
-	private String strategyState = "random";
-	private MainActivity main;
+	private boolean attackSuccess;
+	final int random = 0;
+	final int directionFind = 1;
+	final int directionFollow = 2;
+	final int directionChange = 3;
+	final int north = 0;
+	final int east = 1; 
+	final int south = 2;
+	final int west = 3;	
+	private int strategyState = 0;
 	
 	//TODO: need to create computer ability to set ships.	
 	
-	public void computerMove() { // this is the "main" function to facilitate CPU logic
-		
-		if (useSmartMove == false) {
-			getCpuRandomMove();
+	public void computerMove(int shipSunk) { 	// this is the "main" function to facilitate CPU logic
+												// needs to be passed an int: 0 for ship not sunk, 1 for ship sunk
+		if (shipSunk == 1){						// this will reset the CPU state to random guess attack
+			strategyState = random;
 		}
-		else{
-			cpuSmartMove();
+		
+		
+		switch (strategyState){
+			case random:
+				getCpuRandomMove();
+				break;
+			
+			case directionFind:
+				setCpuDirection();
+				
+				break;
+				
+			case directionFollow:
+				continueAttack();
+				
+				break;
+			case directionChange:
+				changeAttackDirection();
+				computerMove(0);
+				
+					
 		}
 		
 		
 		String move = getMove(x, y);
-		checkForHit(MainActivity.player1ShipArray, move);
+		boolean hitSuccess = checkForHit(MainActivity.player1ShipArray, move);
+		strategyAssign(hitSuccess, strategyState);
 	}
 	
-	private  void getCpuRandomMove(){
+	private void changeAttackDirection() {
+		
+		lastX = firstHitX;
+		lastY = firstHitY;
+		switch (direction){
+			case north: direction = south;
+				break;
+			case east: direction = west;
+				break;
+			case south: direction = north;
+				break;
+			case west: direction = east;
+				break;
+		}
+		
+		continueAttack();
+		strategyState = directionFollow;
+	}
+
+	private void continueAttack() {
+		switch (direction){
+		case north:
+			
+			attackSuccess = attack(north);
+			if (attackSuccess == false){
+				changeAttackDirection();
+			}
+			break;
+			
+		case east:
+			
+			attackSuccess = attack(east);
+			if (attackSuccess == false){
+				changeAttackDirection();
+			}
+			break;	
+			
+		case south:
+			
+			attackSuccess = attack(south);
+			if (attackSuccess == false){
+				changeAttackDirection();
+			}
+			break;	
+				
+		case west:
+			
+			attackSuccess = attack(west);
+			if (attackSuccess == false){
+				changeAttackDirection();
+			}
+			break;	
+		}
+			
+		
+	}
+
+	private void strategyAssign(boolean hitSuccess, int strategyState2) {
+		
+		switch (strategyState){
+		case random:
+			if (hitSuccess = true){
+				strategyState = directionFind;
+			}
+			break;
+		
+		case directionFind:
+			if (hitSuccess = true){
+				strategyState = directionFollow;
+			}
+			
+			break;
+		/*	
+		 *  THIS SWITCH HANDLED IN CONTINUE ATTACK METHOD
+		case directionFollow:
+			if (hitSuccess = false){
+				strategyState = directionChange;
+			}
+			break;
+		/*	
+		 * DIRECTION CHANGE SHOULD NOT BE A VALID CASE AT THIS POINT IN CODE *
+		case directionChange:
+			if (hitSuccess = true){
+				strategyState = directionFollow;
+			}
+			
+			break;
+			
+		*/
+	}
+		
+	}
+
+	private void getCpuRandomMove(){
 		x = generator.nextInt(10);
 		y = generator.nextInt(10)+1;
 
 	}
-	
-
 	
 	private  String getMove(int x, int y){
 		
@@ -81,7 +209,6 @@ public class CPUPlayer extends Activity{
 		return id += Integer.toString(y);
 		}
 
-
 	private boolean checkForHit (ArrayList<String> Array, String move)
 	{
 				
@@ -91,11 +218,10 @@ public class CPUPlayer extends Activity{
 			Log.v("msg", "move = ");
 			if (move.equals(Array.get(i)))
 			{
-				hitX = x;
-				hitY = y;
+				firstHitX = x;
+				firstHitY = y;
 				lastX = x;
 				lastY = y;
-				useSmartMove = true;
 				return true;
 			}
 		}
@@ -104,92 +230,120 @@ public class CPUPlayer extends Activity{
 		
 	}
 	
-	private  void cpuSmartMove(){
+	private void setCpuDirection(){
 		
 		Random generator2 = new Random (4);
 		int guess = generator2.nextInt();
 		
-		if (orientation.equals("vertical")){ 	//once ship orientation is set, this changes the random
-			if ( (guess + 2) % 2 == 1){			//number to modify only vertical (even) cases 0 or 2
-				guess--;
-			}
-		}
-		if (orientation.equals("horizontal")){	//once ship orientation is set, this changes the random
-			if ( (guess + 2) % 2 == 0){			//number to modify only horizontal (odd) cases 1 or 2
-					guess--;
-			}
-		}
-		
-		
 		if (hitGuesses[guess] == 1) { // if the random guess has already been tried, gets new random number
-			cpuSmartMove();
+			setCpuDirection();
 		}
 		
 		else {
 			hitGuesses[guess] = 1; // modifies array to show that the try was attempted.
 		
 			switch (guess){
-			case 0: // test top adjacent space
+			case north: // test top adjacent space
 				
-				y = lastY - 1;
-				if ( y >= 0 ) { 
-					getMove( x , y);
-					lastX = x;
-					lastY = y;
-					orientation = "vertical";
+				attackSuccess = attack(north);
+				if (attackSuccess == false){
+					setCpuDirection();
 				}
-				else {
-					cpuSmartMove();
-				}
-				
 				break;
 			
-			case 1: // test right adjacent space
+			case east: // test right adjacent space
 				
-				x = lastX + 1;
-				if ( x <= 10 ) { 
-					getMove( x , y);
-					lastX = x;
-					lastY = y;
-					orientation = "horizontal";
+				attackSuccess = attack(east);
+				if (attackSuccess == false){
+					setCpuDirection();
 				}
-				else {
-					cpuSmartMove();
-				}
-				
 				break;
 				
-			case 2: // test bottom adjacent space
+			case south: // test bottom adjacent space
 				
-				y = lastY + 1;
-				if ( y <= 10 ) { 
-					getMove( x , y);
-					lastX = x;
-					lastY = y;
-					orientation = "vertical";
+				attackSuccess = attack(south);
+				if (attackSuccess == false){
+					setCpuDirection();
 				}
-				else {
-					cpuSmartMove();
-				}
-				break;	
+				break;
 			
-			case 3: // test left adjacent space
+			case west: // test left adjacent space
 				
-				x = lastX - 1;
-				if ( x >= 0 ) { 
-					getMove( x , y);
-					lastX = x;
-					lastY = y;
-					orientation = "horizontal";
-				}
-				else {
-					cpuSmartMove();
+				attackSuccess = attack(west);
+				if (attackSuccess == false){
+					setCpuDirection();
 				}
 				break;
 			}
 			
-			lastCase = guess;
+			direction = guess;
 		}
 	}
+	
+	private boolean attack(int direction){
+		boolean attackCompleted = false;
+		switch (direction){
+		case north: // test top adjacent space
+			
+			y = lastY - 1;
+			if ( y >= 0 ) { 
+				getMove( x , y);
+				lastX = x;
+				lastY = y;
+				attackCompleted = true;
+				
+			}
+			else {
+				attackCompleted = false;
+			}
+			
+			
+		
+		case east: // test right adjacent space
+			
+			x = lastX + 1;
+			if ( x <= 10 ) { 
+				getMove( x , y);
+				lastX = x;
+				lastY = y;
+				attackCompleted = true;
+				
+			}
+			else {
+				attackCompleted = false;
+			}
+			
+			
+		case south: // test bottom adjacent space
+			
+			y = lastY + 1;
+			if ( y <= 10 ) { 
+				getMove( x , y);
+				lastX = x;
+				lastY = y;
+				attackCompleted = true;
+				
+			}
+			else {
+				attackCompleted = false;
+			}
+		
+		case west: // test left adjacent space
+			
+			x = lastX - 1;
+			if ( x >= 0 ) { 
+				getMove( x , y);
+				lastX = x;
+				lastY = y;
+				attackCompleted = true;
+				
+			}
+			else {
+				attackCompleted = false;
+			}
+		}
+		return attackCompleted;
+	}
+	
 }
 
